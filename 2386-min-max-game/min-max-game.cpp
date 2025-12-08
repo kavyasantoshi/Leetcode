4cbb999e7ac25a709f228a90516ccb1f85c6fed6
kavyasantoshi <kavyasanthoshikorla@gmail.com>
2025-12-08 15:33:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (59.23%) - LeetSync

---
+++ 2386-min-max-game/min-max-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minMaxGame(vector<int>& nums) {
+        int n = nums.size();
+         if(n==2)
+            {
+              return min(nums[0],nums[1]);
+            }
+        while(n!=1)
+        {
+            for(int i=0;i<n/2;i++)
+           {
+            if(i%2==0)
+            {
+                nums[i]=min(nums[2*i],nums[2*i+1]);
+            }
+            else
+            {
+                nums[i]=max(nums[2*i],nums[2*i+1]);
+            }
+           }
+           n=n/2;
+        }
+        return nums[0];
+    }
+};
