68f294497cd94cbd831fa7d3b3e8f5b9bd8d8aca
kavyasantoshi <kavyasanthoshikorla@gmail.com>
2025-12-11 21:05:43 +0530

    Time: 0 ms (100.00%) | Memory: 48.9 MB (72.83%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n = fruits.size();
-        int count=0;
-        int type=0;
-        int result;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(baskets[j]>=fruits[i])
                 {
-                   count++;
                    baskets[j]=0;
                    fruits[i]=0;
                    break;
