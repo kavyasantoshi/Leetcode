d771fd170a36c8f8eb0218bb2985bd427e3f2a59
kavyasantoshi <kavyasanthoshikorla@gmail.com>
2025-12-18 10:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (14.54%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -5,40 +5,17 @@ public:
         int carry = 0;
         for(int i = n-1;i>=0;i--)
         {
-          if(digits[i]%10>=0 && digits[i]%10<=8)
-          {
-            if(carry)
-            {
-               digits[i]+=carry;
-               carry=0;
-               break;
-            }
-            else
-            {
-               digits[i]+=1;
-               break;
-            }
-          }
-        else
-          {
-              if(carry)
-              {
-                 int num = digits[i]+carry;
-                 digits[i] = num%10;
-                 carry = num/10;
-              }
-              else
-              {
-                 int num = digits[i]+1;
-                 digits[i] = num%10;
-                 carry = num/10;
-              }
-          }
+           if(digits[i]==9)
+           {
+              digits[i]=0;
+           }
+           else
+           {
+             digits[i]=digits[i]+1;
+             return digits;
+           }
         }
-        if(carry)
-        {
-            digits.insert(digits.begin(), carry);
-        }
+        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
