4260fc96b8ab974d2997695bb7c9d494b2733c78
KORLA KAVYA SANTOSHI <kavyasanthoshikorla@gmail.com>
2026-08-06 10:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (54.32%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i=0;
+        int j=n-1;
+        while(i<j)
+        {
+            if(!isalnum(s[i]))
+            {
+                i++;
+            }
+             else if(!isalnum(s[j]))
+            {
+                j--;
+            }
+            else
+            {
+             if(tolower(s[i])!=tolower(s[j]))
+             {
+                return false;
+             }
+             i++;
+             j--;
+            }
+        }
+        return true;
+    }
+};
