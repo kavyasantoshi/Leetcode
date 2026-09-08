6ab8acb8931768b48b1ff89c008e6bc9e74e82a6
KORLA KAVYA SANTOSHI <kavyasanthoshikorla@gmail.com>
2026-09-08 10:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (5.52%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        string s = to_string(n);
+        int k = s.size();
+        if(k<=3) return 0;
+        else return (n-999);
+    }
+};
